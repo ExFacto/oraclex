10e7b8fe34202552c8827fd27d2560c499e48aeb
Sachin Meier <sachin.meier@river.com>
2023-04-16 18:39:51 -0400

    rm umbrella structure & client app

--- client/oracle_client/test/oracle_client_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule OracleClientTest do
-  use ExUnit.Case
-  doctest OracleClient
-
-  test "greets the world" do
-    assert OracleClient.hello() == :world
-  end
-end
--- client/oracle_client_lib/test/oracle_client_lib_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule OracleClientLibTest do
-  use ExUnit.Case
-  doctest OracleClientLib
-
-  test "greets the world" do
-    assert OracleClientLib.hello() == :world
-  end
-end
