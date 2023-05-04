0538c7a499e78cc18c4361cfb9ff3b0e266012a4
Sachin Meier <sachin.meier@river.com>
2023-05-04 18:35:28 -0400

    fix event outcome fields

--- lib/oraclex_web/views/event_view.ex
+++ lib/oraclex_web/views/event_view.ex
@@ -49,7 +49,7 @@ defmodule OraclexWeb.EventView do
     content_tag :li, class: "form-field-input-list" do
       [
         apply(Phoenix.HTML.Form, type, [form, field, input_opts]),
-        link(to: "#", data: data, title: "Remove", class: "py-2.5 px-3 ml-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700") do
+        link(to: "#", data: data, title: "Remove", class: "form-list-remove-field py-2.5 px-3 ml-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700") do
           remove_x()
         end
       ]
